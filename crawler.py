afff8c114486900a816d857c7a1873d2e7a85018
Renato Ferreira <renatoferreira249@gmail.com>
2017-10-03 06:32:24 -0300

    teste com google e facebook

--- crawler.py
+++ crawler.py
@@ -6,6 +6,16 @@ def getLinks(page):
     bsObj = BeautifulSoup(page.read(),'html.parser')
     return [ x.get('href') for x in bsObj.find_all('a') ]
 
+class Node:
+    
+    def __init__(self, url, urlsApontadas):
+        self.url = url
+        self.urlsApontadas = urlsApontadas
+
+    def printInfo(self):
+        print(self.url)
+        for url in self.urlsApontadas:
+            print(url)
 """
 page = urlopen('http://www.google.com')
 bsObj = BeautifulSoup(page.read(),'html.parser')
@@ -13,16 +23,17 @@ bsObj = BeautifulSoup(page.read(),'html.parser')
 for link in bsObj.find_all('a'):
     print(link.get('href'))
 """
-urls = [x.strip() for x in open(lista).readlines()]
-nodes = []
 
-for url in sites:
+"""
+urls = [x.strip() for x in open(lista).readlines()]
+"""
+
+nodes = []
+urls = [ 'https://google.com', 'https://facebook.com' ]
+for url in urls:
     page = urlopen(url)
     nodes.append(Node(url,getLinks(page)))    
     inutil = math.cos(math.pi)
 
-class Node:
-    
-    def __init__(self, url, urlsApontadas):
-        self.url = url
-        self.urlsApontadas = urlsApontadas
+for node in nodes:
+    node.printInfo()
